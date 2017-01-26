30f9695745fd78d24b757854be627c61a77b85be
David Rodriguez Fuentes <davidrf.developer@gmail.com>
2017-01-26 12:56:58 -0500

    update repository for the web

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React from 'react'
-import {
-  Text,
-  View,
-  Button
-} from 'react-native';
 
 
 const style = {
@@ -13,21 +8,21 @@ const style = {
 
 const ClickedButton = (props) => {
   return (
-    <Button
-      title={props.clicked ? `${props.title} clicked!` : props.title} onPress={props.onPress}
-    />
+    <button onClick={props.onPress} >
+    {props.clicked ? `${props.title} clicked!` : props.title}
+    </button>
   )
 }
 
 const App = (props) => {
   return (
-  <View style={style}>
+  <div style={style}>
     <ClickedButton title="Add One" onPress={props.addOne} clicked={'Add One' === props.lastClicked}/>
     <ClickedButton title="Substract One" onPress={props.substractOne} clicked={'Substract One' === props.lastClicked}/>
     <ClickedButton title="Reset" onPress={props.reset} clicked={'Reset' === props.lastClicked}/>
 
-    <Text>{props.count}</Text>
-  </View>
+    <h1>{props.count}</h1>
+  </div>
  )
 }
 
